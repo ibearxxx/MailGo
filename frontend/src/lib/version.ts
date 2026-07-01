6c8bfc0616607a9998ea6b9529831dafa431e252
MengMengCode <admin@ai.edu.kg>
2026-07-01 22:17:21 +0800

    release: v0.2.4

--- frontend/src/lib/version.ts
+++ frontend/src/lib/version.ts
@@ -1,3 +1,3 @@
-export const APP_VERSION = "0.2.3";
+export const APP_VERSION = "0.2.4";
 export const REPOSITORY_NAME = "MengMengCode/MailGo";
 export const REPOSITORY_URL = "https://github.com/MengMengCode/MailGo";
